63baabd661bd471cb999086f7c8d320a93ad6e95
Siddharth <siddharth@nearnow.com>
2023-01-15 18:46:22 +0530

    Initial implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { App, message, Button, Upload } from 'antd';
+import { PlusOutlined, FileExcelOutlined, DownloadOutlined } from '@ant-design/icons';
+import AppService from './service.js';
+import * as XLSX from "xlsx";
+import * as _ from 'underscore';
 
-function App() {
+const MyPage = () => {
+  const { message, notification, modal } = App.useApp();
+  const [processing, setIsProcessing] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const [workbook, setWorkbook] = useState();
+  const [file, setFile] = useState();
+
+
+
+
+  const handleChange = async (info) => {
+    setFile(info.file);
+
+    const fileReader = await new FileReader()
+    fileReader.readAsArrayBuffer(info.file.originFileObj)
+    fileReader.onload = (e) => {
+      const bufferArray = e?.target.result
+      const workbook = XLSX.read(bufferArray, { type: "buffer" })
+      setWorkbook(workbook)
+    }
+  };
+
+  const processDocument = () => {
+    setIsProcessing(true);
+    AppService.startProcessing(workbook)
+      .then(() => {
+        setIsProcessing(false);
+        setFinished(true);
+      });
+  }
+
+  const downloadOutputDocument = () => {
+    AppService.downloadProcessedFile();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <div>
+        <div style={{ fontSize: 25, fontWeight: 'bold' }}>
+          Automator
+        </div>
+        <ExcelUploadButton handleChange={handleChange} file={file} />
+
+        {
+          finished ?
+            <div style={{ paddingBottom: 10, paddingTop: 20 }}>
+              <Button type="primary" onClick={downloadOutputDocument} icon={<DownloadOutlined />} size={30}>
+                Download
+              </Button>
+            </div> : null
+        }
+
+
+        <ProcessButton
+          file={file}
+          finished={finished}
+          processing={processing}
+          processDocument={processDocument}
+        />
+
+      </div>
+    </div>
+  );
+};
+
+const MyApp = () => (
+  <App>
+    <MyPage />
+  </App>
+);
+
+export default MyApp;
+
+const ProcessButton = ({ file, finished, processing, processDocument }) => {
+
+  if (file && !finished) {
+    return <div style={{ paddingBottom: 10, paddingTop: 15 }}>
+      <Button type="primary"
+        loading={processing}
+        onClick={processDocument}>
+        Process
+      </Button>
+    </div>;
+  }
+
+  return <div />;
+
+}
+
+
+const ExcelUploadButton = ({ handleChange, file }) => {
+  const [loading, setLoading] = useState(false);
+
+  const uploadButtonUI = (
+    <div>
+      {loading ? <div /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  )
+
+  const UploadedFileInfo = () => {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <FileExcelOutlined style={{ fontSize: 50 }} />
+        {file.name}
+      </div>
+    )
+  }
+
+  return (
+    <div style={{ minWidth: 200 }}>
+      <div style={{ fontSize: 18, paddingBottom: 10, paddingTop: 20 }}>
+        Upload Source Excel File
+      </div>
+      <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        maxCount={1}
+        showUploadList={false}
+        onChange={handleChange}
+        customRequest={() => { }}
+      >
+        <div >
+          {file ? <UploadedFileInfo /> : uploadButtonUI}
+        </div>
+      </Upload>
     </div>
   );
 }
-
-export default App;
---
+++ src/service.js
@@ -0,0 +1,94 @@
+import * as XLSX from "xlsx";
+import * as _ from 'underscore';
+
+function trimmed(stringData) {
+  return ('' + stringData).trim();
+}
+
+function parseNumber(numString) {
+  return (numString + '').replace(/[^0-9.-]+/g, "")
+}
+
+
+class AppService {
+  static outputWorkbook = null;
+  static outputJsonData = null;
+
+  static async initProcessing(sourceWorkbook) {
+    var clicksWorksheet = sourceWorkbook.Sheets[sourceWorkbook.SheetNames[0]];
+    var tenderWorksheet = sourceWorkbook.Sheets[sourceWorkbook.SheetNames[1]];
+    var egcmcWorksheet = sourceWorkbook.Sheets[sourceWorkbook.SheetNames[2]];
+
+    let clicksJsonData = XLSX.utils.sheet_to_json(clicksWorksheet);
+    let tendersJsonData = XLSX.utils.sheet_to_json(tenderWorksheet);
+    let egcmcJsonData = XLSX.utils.sheet_to_json(egcmcWorksheet);
+
+    function initMatchingProcess(accountNo, amount) {
+      var isMatched = false;
+
+      _.each(tendersJsonData, (eachData) => {
+        let _acct = trimmed(eachData['Document #']);
+        let _amount = parseNumber(eachData['Tender Amount']);
+        if (accountNo == _acct && amount == -1 * _amount) {
+          isMatched = true;
+        }
+      });
+
+      if (!isMatched) {
+        _.each(egcmcJsonData, (eachData) => {
+          let _acct = trimmed(eachData['Acct']);
+          let _amount = parseNumber(eachData['Amount']);
+          if (accountNo == _acct && amount == _amount) {
+            isMatched = true;
+          }
+        });
+      }
+
+      return isMatched;
+    }
+
+    function initMatchingWorkflow() {
+      let foundCount = 0;
+
+      _.each(clicksJsonData, (eachData, index) => {
+        let isMatched = false;
+        let accountName = trimmed(eachData['Acct']);
+        let amountPrice = parseNumber(eachData['Amount']);
+
+        isMatched = initMatchingProcess(accountName, amountPrice);
+
+        if (isMatched) {
+          foundCount += 1;
+          eachData['Matched'] = trimmed(eachData['Acct']) + trimmed(eachData['Amount']);
+        } else {
+          eachData['Matched'] = '#N/A';
+        }
+      })
+
+      console.log(`Matched ${foundCount} out of ${_.values(clicksJsonData).length} entries in this file`);
+      AppService.outputJsonData = clicksJsonData;
+    }
+
+
+    initMatchingWorkflow();
+  }
+
+  static startProcessing(sourceWorkbook) {
+    return new Promise(async (resolve) => {
+      setTimeout(function () {
+        AppService.initProcessing(sourceWorkbook).then(() => {
+          resolve();
+        });
+      }, 1000);
+    });
+  }
+
+  static downloadProcessedFile() {
+    AppService.outputWorkbook = XLSX.utils.book_new();
+    let outputWorksheet = XLSX.utils.json_to_sheet(AppService.outputJsonData);
+    XLSX.utils.book_append_sheet(AppService.outputWorkbook, outputWorksheet, "Output");
+    XLSX.writeFileXLSX(AppService.outputWorkbook, 'output.xlsx');
+  }
+}
+
+export default AppService;
